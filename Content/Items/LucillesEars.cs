f55c0ba7cbe1702ab1a2c932d268680a1e8cfae2
Dominic Karma <farfetchedking541@gmail.com>
2024-11-25 03:03:56 -0600

    Add ear twitching and squishing

--- Content/Items/LucillesEars.cs
+++ Content/Items/LucillesEars.cs
@@ -1,4 +1,5 @@
-﻿using Luminance.Core.Graphics;
+﻿using Luminance.Common.Easings;
+using Luminance.Common.Utilities;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
@@ -46,18 +47,18 @@ namespace SylvVanity.Content.Items
 
         private static void DrawEars(Player drawPlayer)
         {
+            if (!drawPlayer.TryGetModPlayer(out SylvVanityPlayer vanityPlayer))
+                return;
+
             Vector2 earDrawPosition = drawPlayer.TopLeft - Main.screenPosition + Vector2.UnitY * drawPlayer.gfxOffY;
             earDrawPosition += new Vector2((drawPlayer.width - drawPlayer.bodyFrame.Width) / 2f, drawPlayer.height - drawPlayer.bodyFrame.Height + 4f);
             Vector2 headVect = new(drawPlayer.legFrame.Width * 0.5f, drawPlayer.legFrame.Height * 0.4f);
             earDrawPosition += new Vector2(drawPlayer.direction == 1 ? 1f : -11f, -4f);
             earDrawPosition += drawPlayer.headPosition + headVect;
-
-            Texture2D ears = ModContent.Request<Texture2D>("SylvVanity/Content/Items/LucillesEars").Value;
-            ManagedShader pixelationShader = ShaderManager.GetShader("Luminance.PixelationShader");
-            pixelationShader.TrySetParameter("pixelationFactor", Vector2.One * 2f / ears.Size());
-            pixelationShader.Apply();
-
             earDrawPosition.Y = MathF.Round(earDrawPosition.Y / 2f) * 2f;
+
+            Texture2D leftEar = ModContent.Request<Texture2D>("SylvVanity/Content/Items/LucillesEarLeft").Value;
+            Texture2D rightEar = ModContent.Request<Texture2D>("SylvVanity/Content/Items/LucillesEarRight").Value;
 
             Rectangle hairFrame = drawPlayer.bodyFrame;
             hairFrame.Y -= 336;
@@ -65,9 +66,29 @@ namespace SylvVanity.Content.Items
             if (hairFrame.Y == 56 || hairFrame.Y == 112 || hairFrame.Y == 168 || hairFrame.Y == 448 || hairFrame.Y == 504 || hairFrame.Y == 560)
                 earDrawPosition.Y -= 2f;
 
-            // Draw ears.
+            Vector2 leftEarDrawPosition = earDrawPosition.Floor() + Vector2.UnitX * (drawPlayer.direction == 1 ? -4f : 14f);
+            Vector2 rightEarDrawPosition = leftEarDrawPosition + Vector2.UnitX * drawPlayer.direction * 12f;
+
+            // Calculate the base ear rotation.
             float earRotation = drawPlayer.headRotation + drawPlayer.direction * -0.16f;
-            Main.spriteBatch.Draw(ears, earDrawPosition.Floor(), null, Color.White, earRotation, new Vector2(headVect.X, ears.Height), 0.37f, drawPlayer.direction == 1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally, 0);
+
+            // Calculate the angular offset of ears based on twitch.
+            float twitchAngleOffset = Utilities.Convert01To010(EasingCurves.Evaluate(EasingCurves.Cubic, EasingType.Out, vanityPlayer.EarTwitchAnimationCompletion)) * drawPlayer.direction * 0.26f;
+
+            float leftEarRotation = earRotation - twitchAngleOffset;
+            float rightEarRotation = earRotation + twitchAngleOffset * 0.04f;
+
+            // Calculate the size of the ears.
+            Vector2 earScale = Vector2.One * 0.42f;
+
+            float squish = Utilities.Cos01(drawPlayer.Center.X * 0.017f) * Utilities.InverseLerp(4f, 19.5f, drawPlayer.velocity.Length()) * 0.25f;
+            earScale.X *= 1f - squish;
+            earScale.Y *= 1f + squish * 1.4f;
+
+            // Draw ears.
+            SpriteEffects earDirection = drawPlayer.direction == 1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
+            Main.spriteBatch.Draw(leftEar, leftEarDrawPosition, null, Color.White, leftEarRotation, new Vector2(leftEar.Width * 0.5f, leftEar.Height), earScale, earDirection, 0);
+            Main.spriteBatch.Draw(rightEar, rightEarDrawPosition, null, Color.White, rightEarRotation, new Vector2(rightEar.Width * 0.5f, rightEar.Height), earScale, earDirection, 0);
         }
 
         public override void SetDefaults()
--- SylvVanityPlayer.cs
+++ SylvVanityPlayer.cs
@@ -1,3 +1,4 @@
+using Terraria;
 using Terraria.ModLoader;
 
 namespace SylvVanity
@@ -11,6 +12,28 @@ namespace SylvVanity
         {
             get;
             set;
+        }
+
+        /// <summary>
+        /// A 0-1 animation completion interpolation for ear twitches.
+        /// </summary>
+        public float EarTwitchAnimationCompletion
+        {
+            get;
+            set;
+        }
+
+        public override void PreUpdateMovement()
+        {
+            // Random start the ear twich animation.
+            if (Player.velocity.Length() <= 1f && Main.rand.NextBool(120) && EarTwitchAnimationCompletion <= 0f)
+                EarTwitchAnimationCompletion = 0.01f;
+
+            // Update animations.
+            if (EarTwitchAnimationCompletion > 0f)
+                EarTwitchAnimationCompletion += 0.05f;
+            if (EarTwitchAnimationCompletion >= 1f)
+                EarTwitchAnimationCompletion = 0f;
         }
     }
 }
