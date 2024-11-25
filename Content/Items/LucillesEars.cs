56af34f975d1dc7325881b24fe97c7ef2014885b
Dominic Karma <farfetchedking541@gmail.com>
2024-11-25 01:21:39 -0600

    Initial commit

---
+++ Content/Items/LucillesEars.cs
@@ -0,0 +1,25 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SylvVanity.Content.Items
+{
+    [AutoloadEquip(EquipType.Head)]
+    public class LucillesEars : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            if (Main.netMode != NetmodeID.Server)
+                ArmorIDs.Head.Sets.DrawHead[Item.headSlot] = false;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 72;
+            Item.height = 80;
+            Item.scale = 0.5f;
+            Item.rare = ItemRarityID.Pink;
+            Item.vanity = true;
+        }
+    }
+}
---
+++ Content/Items/LucillesEarsLayer.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SylvVanity.Content.Items
+{
+    public class LucillesEarsLayer : PlayerDrawLayer
+    {
+        public override Position GetDefaultPosition() => new AfterParent(PlayerDrawLayers.Head);
+
+        public override bool GetDefaultVisibility(PlayerDrawSet drawInfo) => drawInfo.shadow == 0f || !drawInfo.drawPlayer.dead;
+
+        protected override void Draw(ref PlayerDrawSet drawInfo)
+        {
+            Player drawPlayer = drawInfo.drawPlayer;
+            Item headItem = drawPlayer.armor[0];
+
+            if (drawPlayer.armor[10].type > ItemID.None)
+                headItem = drawPlayer.armor[10];
+
+            if (headItem.type == ModContent.ItemType<LucillesEars>())
+            {
+                string equipSlotName = headItem.ModItem.Name;
+                int equipSlot = EquipLoader.GetEquipSlot(Mod, equipSlotName, EquipType.Head);
+
+                if (!drawInfo.drawPlayer.dead && equipSlot == drawPlayer.head)
+                {
+                    int dyeShader = drawPlayer.dye?[0].dye ?? 0;
+
+                    // It is imperative to use drawInfo.Position and not drawInfo.Player.Position, or else the layer will break on the player select & map (in the case of a head layer).
+                    Vector2 headDrawPosition = drawInfo.Position - Main.screenPosition;
+                    headDrawPosition += new Vector2((drawPlayer.width - drawPlayer.bodyFrame.Width) / 2f, drawPlayer.height - drawPlayer.bodyFrame.Height + 4f);
+
+                    // Floor the draw position to remove jitter.
+                    headDrawPosition = headDrawPosition.Floor();
+
+                    headDrawPosition += drawPlayer.headPosition + drawInfo.headVect;
+
+                    // Grab the extension texture.
+                    Texture2D extraPieceTexture = ModContent.Request<Texture2D>(ModContent.GetInstance<LucillesEars>().Texture).Value;
+
+                    DrawData pieceDrawData = new(extraPieceTexture, headDrawPosition, null, drawInfo.colorArmorHead, drawPlayer.headRotation, drawInfo.headVect, 0.5f, drawInfo.playerEffect, 0)
+                    {
+                        shader = dyeShader
+                    };
+
+                    drawInfo.DrawDataCache.Add(pieceDrawData);
+                }
+            }
+        }
+    }
+}
